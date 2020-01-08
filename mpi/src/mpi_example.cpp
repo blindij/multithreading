4bb1deda0464c5e9a983ad1488bef8137ae6ba43
Bjorn Lindi <blindij@users.noreply.github.com>
2020-01-08 08:43:21 +0100

    Show an hello_mpi example.

---
+++ mpi/src/mpi_example.cpp
@@ -0,0 +1,33 @@
+// File: mpi_example.cpp
+//
+// hello_mpi.cpp from http://www.mpitutorial.com
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char **argv){
+   // Initialize the MPI environment. The two arguments to MPI Init are not
+   // currently used by MPI implementations, but are there in case future
+   // implementations might need the arguments.
+   MPI_Init(NULL,NULL);
+
+   // Get the number of processes
+   int world_size;
+   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+   // Get the rank of the process
+   int world_rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+   // Get the name of the processor
+   char processor_name[MPI_MAX_PROCESSOR_NAME];
+   int name_len;
+   MPI_Get_processor_name(processor_name, &name_len);
+
+   // Print of a hello world message
+   std::cout << "Hello world from processor " << processor_name << ", rank "
+             << world_rank << " out of " << world_size << " processors" << std::endl;
+
+   // Finalize the MPI environment.
+   MPI_Finalize();
+   return 0;
+}
